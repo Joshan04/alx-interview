742514e43819517d7eb89360f79f6cb5d00e5bf4
Joshan04 <boluwatifejoshua4@gmail.com>
2023-08-20 14:26:32 -0400

    PARS

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,45 @@
 #!/usr/bin/python3
 
-
-"""
-This module holds a simple module to do log
-parsing. Given a input of log files, extract some
-usefull information and print it to standard output
-"""
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
-import re
-def check_log_format(log_string):
-    regex_pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d+'
-    return re.match(regex_pattern, log_string)
 
 
-def print_to_stdout(**kwargs):
-    """
-    A simple helper function to print
-    the values to standard output
-    """
-    for key in kwargs.keys():
-        if not isinstance(kwargs.get(key), dict):
-            # Means this is not a dictionary of dictionaries
-            # So Its the file size
-            print("File Size {}".format(kwargs.get(key)))
-        else:
-            for nested_k, nested_v in key.items():
-                print(f"{nested_k}: {nested_v}")
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-def read_and_analyze_log_files():
-    while True:
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
         try:
-            status_codes = {
-                "200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "403": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
+            size += int(stlist[-1])
+        except:
+            pass
 
-            }
-            total_size = 0
-            for time in range(11):
-                log_info = input("").strip()
-                if check_log_format(log_info):
-                    log_info_list = log_info.split(" ")
-                    f_size = log_info_list[-1]
-                    s_code = log_info_list[-2]
-                    status_codes[s_code] = status_codes.get(s_code) + 1
-                    total_size += int(f_size)
-                    sorted_keys_dict = {code: status_codes[code] for code in sorted(status_codes)}
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
-        except KeyboardInterrupt:
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
+
+
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
